781ae1e301174f743b05bb59838a58f11740d248
Zain <zai1999n@gmail.com>
2020-12-21 00:49:10 +0200

    Updated App.js .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+    import Modal from "./components/Modal";
+
     const todoItems = [
       {
         id: 1,
@@ -29,10 +31,33 @@ import React, { Component } from "react";
       constructor(props) {
         super(props);
         this.state = {
+          modal: false,
           viewCompleted: false,
+          activeItem: {
+            title: "",
+            description: "",
+            completed: false
+          },
           todoList: todoItems
         };
       }
+      toggle = () => {
+        this.setState({ modal: !this.state.modal });
+      };
+      handleSubmit = item => {
+        this.toggle();
+        alert("save" + JSON.stringify(item));
+      };
+      handleDelete = item => {
+        alert("delete" + JSON.stringify(item));
+      };
+      createItem = () => {
+        const item = { title: "", description: "", completed: false };
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
+      editItem = item => {
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
       displayCompleted = status => {
         if (status) {
           return this.setState({ viewCompleted: true });
@@ -60,8 +85,7 @@ import React, { Component } from "react";
       renderItems = () => {
         const { viewCompleted } = this.state;
         const newItems = this.state.todoList.filter(
-          // eslint-disable-next-line
-          item => item.completed == viewCompleted
+          item => item.completed === viewCompleted
         );
         return newItems.map(item => (
           <li
@@ -77,8 +101,18 @@ import React, { Component } from "react";
               {item.title}
             </span>
             <span>
-              <button className="btn btn-secondary mr-2"> Edit </button>
-              <button className="btn btn-danger">Delete </button>
+              <button
+                onClick={() => this.editItem(item)}
+                className="btn btn-secondary mr-2"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => this.handleDelete(item)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </span>
           </li>
         ));
@@ -91,7 +125,9 @@ import React, { Component } from "react";
               <div className="col-md-6 col-sm-10 mx-auto p-0">
                 <div className="card p-3">
                   <div className="">
-                    <button className="btn btn-primary">Add task</button>
+                    <button onClick={this.createItem} className="btn btn-primary">
+                      Add task
+                    </button>
                   </div>
                   {this.renderTabList()}
                   <ul className="list-group list-group-flush">
@@ -100,6 +136,13 @@ import React, { Component } from "react";
                 </div>
               </div>
             </div>
+            {this.state.modal ? (
+              <Modal
+                activeItem={this.state.activeItem}
+                toggle={this.toggle}
+                onSave={this.handleSubmit}
+              />
+            ) : null}
           </main>
         );
       }
