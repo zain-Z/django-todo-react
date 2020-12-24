6cf1bb5f4a7cb87d4e3034c665483515b46be224
Zain <zai1999n@gmail.com>
2020-12-24 07:06:48 +0200

    Changed sqlite3 to postgreSQL13.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoreact',
+        'USER': 'postgres',
+        'PASSWORD': 'add your password here',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
